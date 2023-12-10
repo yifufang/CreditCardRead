ccfb44a16da38eb90c82b122207387d881579a84
yifufang <yffang01@gmail.com>
2023-12-10 02:51:32 -0800

    Testing

--- runTest.sh
+++ runTest.sh
@@ -2,6 +2,5 @@ classpath="."
 for file in lib/*.jar; do
   classpath="$classpath;$file"
 done
-
 javac -cp "$classpath" ./src/*.java -d ./out
-java -cp "$classpath;./out" Main
+java -cp "$classpath;./out" org.junit.runner.JUnitCore CreditCardTest
--- src/CreditCardTest.java
+++ src/CreditCardTest.java
@@ -1,4 +1,5 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class CreditCardTest {
@@ -6,12 +7,26 @@ public class CreditCardTest {
     @Test
     public void testGetCardNumber() {
         // Arrange
-        CardFactory factory = new ConcreateCardFactory();
-        CreditCard card = factory.createCard("347856341908126");
+        try{
+            CardFactory factory = new ConcreateCardFactory();
+            CreditCard card = factory.createCard("");
+            CreditCard card2 = factory.createCard("12345678901234567890123");
+            CreditCard card3 = factory.createCard("123456789012345678U");
+            CreditCard card4 = factory.createCard("52345678901234*6");
+            CreditCard card5 = factory.createCard("3601112345678789");
+            // Assert
+            assertEquals("Invalid: empty/null card number", card.getCardType());
+            assertEquals("Invalid: more than 19 digits", card2.getCardType());
+            assertEquals("Invalid: contains non-numeric characters", card3.getCardType());
+            assertEquals("Invalid: contains non-numeric characters", card4.getCardType());
+            assertEquals("Invalid: not a possible card number", card5.getCardType());
 
-        // Assert
-        assertEquals("AmericanExpress", card.getCardType());
+            System.out.println("All tests succeeded!");
+        }
+        catch(AssertionError e){
+            throw e;
+        }
+        
     }
-
     // Add more test methods here
 }
